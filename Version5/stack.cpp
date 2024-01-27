1fb9188bcf1e642fbc3b17eaa5366a559a51bfcd
Eric Sison <ericsison.dev@gmail.com>
2024-01-27 10:33:40 +0800

    final instruction

--- Version5/stack.cpp
+++ Version5/stack.cpp
@@ -1,21 +1,38 @@
-#include <cstdio>
+#include <iostream>
 #include "stack.h"
 using namespace std;
 
-// constructor:  initialize top
+// constructor: initialize top
 stack::stack()
 {
+    top = 0;
 }
 
-// push method:  add element to array, increment top
+// push method: add element to array, increment top
 // print "full" if top == MAX
 void stack::push(char x)
 {
+    if (top < MAX)
+    {
+        Store[top++] = x;
+    }
+    else
+    {
+        cout << "full" << endl;
+    }
 }
 
 // pop method: decrement top, then retrieve element
 // print "empty" if top == 0
 char stack::pop()
 {
-	return 0;
+    if (top > 0)
+    {
+        return Store[--top];
+    }
+    else
+    {
+        cout << "empty" << endl;
+        return 0; // makes sure something is returned regardless
+    }
 }
